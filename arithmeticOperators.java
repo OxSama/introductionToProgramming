b241aeb20dde7cc22fc138de406b00b848ac1241
Mohammed <mhmdtajeldin@gmail.com>
2022-11-16 20:37:19 +0200

    introduction to programming lab

---
+++ arithmeticOperators.java
@@ -0,0 +1,13 @@
+public class arithmeticOperators{
+public static void main(String args []){
+
+double number1 = 25;
+
+double number2 = 26;
+
+double number3 = 5;
+
+System.out.print("25/5 = " + number1/number3 + "\n 26/5 = " + number2/number3 + "\n");
+
+}
+}
